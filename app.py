d98eabe6ddb5cb90dd6250349b889ddd4dd5cf98
Prabhath <venkataprabhath.bandi@gmail.com>
2026-03-03 16:54:27 +0530

    fixed app.py after merge

--- app.py
+++ app.py
@@ -14,26 +14,54 @@ def fetch_poster(id):
 
 movies_dict = pickle.load(open('movie_list_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
+<<<<<<< HEAD
 similarity = pickle.load(open('similarity.pkl','rb'))
+=======
+top_similar = pickle.load(open('similarity.pkl','rb'))
+>>>>>>> 82890a6 (fixed app.py after merge)
 
 st.title("Movie Recommendation System")
 
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
+<<<<<<< HEAD
     distances = sorted(
         list(enumerate(similarity[index])),
         reverse=True,
         key=lambda x: x[1]
     )
+=======
+
+    distances = top_similar[index]
+>>>>>>> 82890a6 (fixed app.py after merge)
 
     recommended_movie_names = []
     recommended_movie_posters = []
 
     for i in distances[1:7]:
+<<<<<<< HEAD
         movie_id = movies.iloc[i[0]]['id']
         recommended_movie_posters.append(fetch_poster(movie_id))  # FIXED
         recommended_movie_names.append(movies.iloc[i[0]]['title'])
+=======
+
+        # If tuple → take first element
+        if isinstance(i, (list, tuple)):
+            movie_index = int(i[0])
+        else:
+            movie_index = int(i)
+
+        movie_id = movies.iloc[movie_index]['id']
+
+        recommended_movie_names.append(
+            movies.iloc[movie_index]['title']
+        )
+
+        recommended_movie_posters.append(
+            fetch_poster(movie_id)
+        )
+>>>>>>> 82890a6 (fixed app.py after merge)
 
     return recommended_movie_names, recommended_movie_posters
 
@@ -53,7 +81,11 @@ if st.button('Show Similar Movies 🍿', type='primary'):
         with st.spinner("Finding similar movies..."):
             names, posters = recommend(selected_movie_name)
 
+<<<<<<< HEAD
         # 🔥 Top Padding
+=======
+        # Top Padding
+>>>>>>> 82890a6 (fixed app.py after merge)
         st.markdown("<div style='margin-top:60px;'></div>", unsafe_allow_html=True)
 
         # Wider center area (~85%)
@@ -74,7 +106,11 @@ if st.button('Show Similar Movies 🍿', type='primary'):
                         unsafe_allow_html=True
                     )
 
+<<<<<<< HEAD
         # 🔥 Bottom Padding
+=======
+        # Bottom Padding
+>>>>>>> 82890a6 (fixed app.py after merge)
         st.markdown("<div style='margin-bottom:80px;'></div>", unsafe_allow_html=True)
 
     else:
